e5adbcfbac23ae559c73a67a7b87b02a1b3af528
kieran <kierantmurphy@gmail.com>
2014-08-13 23:51:45 +0100

    Added .gitignore. Removed .pyc files

--- src/django1/urls.py
+++ src/django1/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from myapp.views import HelloTemplateView
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +9,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'myapp.views.hello_template', name='home'),
+    url(r'^hello_simple$', 'myapp.views.hello_simple', name='home'),
+    url(r'^hello_template$', 'myapp.views.hello_template', name='home'),
+    url(r'^hello_template_simple$', 'myapp.views.hello_template_simple', name='home'),
+    url(r'^hello_template_class$', HelloTemplateView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
-def hello(request):
+def hello_simple(request):
     name = "murphyki"
     html = "<html><body>Hi %s. this seems to have worked!</body></html>" % name
     return HttpResponse(html)
@@ -14,3 +15,15 @@ def hello_template(request):
     template = get_template("hello.html")
     html = template.render(Context({'name':name}))
     return HttpResponse(html)
+
+def hello_template_simple(request):
+    name = "murphyki"
+    return render_to_response("hello.html", {'name':name})
+
+class HelloTemplateView(TemplateView):
+    template_name = "hello_class.html"
+    def get_context_data(self, **kwargs):
+        #context = super(HelloTemplateView, self).get_context_data(**kwargs)
+        context = TemplateView.get_context_data(self, **kwargs)
+        context['name']  = "murphyki"
+        return context
